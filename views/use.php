6e5fa04e4473309c6c771a7263045fe0c46301e0
Alexandre Thomazo <alex@thomazo.info>
2012-11-08 20:49:31 +0100

    Adding "use" menu

--- views/use.php
+++ views/use.php
@@ -1,1 +1,15 @@
-<h2>En cours de construction</h2>
+<div class="row">
+	<div class="span2 well" style="padding: 8px 0;">
+		<ul class="nav nav-list">
+			<li class="nav-header">Utilisation</li>
+			<li class="active"><a href="#">Premier lancement</a></li>
+			<li><a href="#">Charges/Revenus</a></li>
+			<li><a href="#">Operations</a></li>
+			<li><a href="#">Synchronisation</a></li>
+		</ul>
+	</div>
+	
+	<div class="span9">
+		Content
+	</div>
+</div>
