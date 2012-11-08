713845bc2ca4c4ac53ed2455882927aa2ba7e618
Alexandre Thomazo <alex@thomazo.info>
2012-11-08 22:53:19 +0100

    Adding contribute page

--- views/contribute.php
+++ views/contribute.php
@@ -1,1 +1,18 @@
-<h2>En cours de construction</h2>
+<h3>Contribuer</h3>
+
+<p>BankIt est un logiciel source sous license <a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GPL v3</a>. A ce titre, vous avez la liberté&nbsp;:</p>
+<ul>
+	<li>d'utiliser le logiciel pour n'importe quel usage</li>
+	<li>de modifier le programme pour répondre à vos besoin</li>
+	<li>de redistribuer des copies</li>
+	<li>de partager vos modifications avec d'autres</li>
+</ul>
+
+<p>Le projet est hébergé sur GitHub : <a href="https://github.com/alexthomazo/bankit" target="_blank">https://github.com/alexthomazo/bankit</a>.</p>
+
+<p>N'hésitez pas à soumettre des <a href="https://github.com/alexthomazo/bankit/pulls" target="_blank">pull-request</a> 
+ou à <a href="https://github.com/alexthomazo/bankit/issues" target="_blank">rapporter des bugs</a>.</p>
+
+<a href="https://github.com/alexthomazo/bankit" target="_blank">
+	<img style="position: absolute; top: 40px; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub">
+</a>
