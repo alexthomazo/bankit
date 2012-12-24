b9fc598cb0096822277563fbc97e33f49693fdd2
Alexandre Thomazo <alex@thomazo.info>
2012-12-24 15:55:50 +0100

    Handle dev channel in update JS callback

--- update.php
+++ update.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+header('Content-Type: application/javascript');
 require('lib.php');
 
+$channel = 'stable';
+if (isset($_GET['channel']) && is_channel_valid($_GET['channel'])) {
+	$channel = $_GET['channel'];
+}
 if (isset($_GET['callback'])) {
 	echo $_GET['callback'] . "(";
 }
-echo "{'commit':'" . get_git_property("git.commit.id") . "'}";
+echo "{'commit':'" . get_git_property("git.commit.id", $channel) . "'}";
 
 if (isset($_GET['callback'])) {
 	echo ");";
